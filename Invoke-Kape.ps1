55e8f904840e401d3b7f36449fe876188ceaf9e2
5f0ne <96919731+5f0ne@users.noreply.github.com>
2025-12-08 16:46:38 +0100

    Added vhdx creation option to Invoke-Kape

--- Invoke-Kape.ps1
+++ Invoke-Kape.ps1
@@ -1,11 +1,18 @@
 param(
     $Path,
-    $OutputPath=".\results\kape",
-    $KapePath="kape.exe",
-    $VhdxIdentifier="kape-collection",
-    $KapeTargets="!BasicCollection,!SANS_Triage,KapeTriage,ServerTriage,CombinedLogs,IRCClients,RecycleBin,RemoteAdmin,SOFELK,SQLiteDatabases,TorrentClients,RecentFileCache,StartupFolders,CloudStorage_All"
+    $OutputPath = ".\results\kape",
+    $Vhdx = $False,
+    $VhdxIdentifier = "kape-collection",
+    $KapePath = "kape.exe",
+    $KapeTargets = "!BasicCollection,!SANS_Triage,KapeTriage,ServerTriage,CombinedLogs,IRCClients,RecycleBin,RemoteAdmin,SOFELK,SQLiteDatabases,TorrentClients,RecentFileCache,StartupFolders,CloudStorage_All"
 )
 
-$param="--tsource $Path --tdest $OutputPath --target $KapeTargets --vhdx $VhdxIdentifier --vss"
+if ($Vhdx) {
+    $param = "--tsource $Path --tdest $OutputPath --target $KapeTargets --vhdx $VhdxIdentifier --vss"
+}
+else {
+    $param = "--tsource $Path --tdest $OutputPath --target $KapeTargets --vss"
+
+}
 
 Start-Process -FilePath $KapePath -ArgumentList $param  -Wait -NoNewWindow -RedirectStandardOutput "$OutputPath\kape-log.txt" -RedirectStandardError "$OutputPath\kape-error.txt"
