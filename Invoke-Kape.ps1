5729152206e3e7eb4a2d4a454a3cc1c79c2cccc6
5f0ne <96919731+5f0ne@users.noreply.github.com>
2025-12-08 15:51:28 +0100

    Invoke-Kape cmd options changed

--- Invoke-Kape.ps1
+++ Invoke-Kape.ps1
@@ -2,9 +2,10 @@ param(
     $Path,
     $OutputPath=".\results\kape",
     $KapePath="kape.exe",
+    $VhdxIdentifier="kape-collection",
     $KapeTargets="!BasicCollection,!SANS_Triage,KapeTriage,ServerTriage,CombinedLogs,IRCClients,RecycleBin,RemoteAdmin,SOFELK,SQLiteDatabases,TorrentClients,RecentFileCache,StartupFolders"
 )
 
-$param="--tsource $Path --tdest $OutputPath --target $KapeTargets"
+$param="--tsource $Path --tdest $OutputPath --target $KapeTargets --vhdx $VhdxIdentifier --vss"
 
 Start-Process -FilePath $KapePath -ArgumentList $param  -Wait -NoNewWindow -RedirectStandardOutput "$OutputPath\kape-log.txt" -RedirectStandardError "$OutputPath\kape-error.txt"
